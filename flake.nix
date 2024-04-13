a46767d180e4a7b4a1d3203e588fe8fad0b41beb
frectonz <fraol0912@gmail.com>
2024-04-13 23:22:38 +0300

    support `nix run`

--- flake.nix
+++ flake.nix
@@ -15,6 +15,15 @@
         pkgs = import nixpkgs { inherit system; };
       in
       with pkgs; {
+        packages.default = go.overrideAttrs (final: prev: {
+          src = fetchFromGitHub {
+            owner = "frectonz";
+            repo = "better-go";
+            rev = "second-try";
+            hash = "sha256-V/2gW9Ig7CkaNz/H7Vt5pUHH8BBPSea5cUkXbKfzhu8=";
+          };
+        });
+
         devShells.default = mkShell.override { stdenv = clangStdenv; } {
           buildInputs = [ go gotools ];
         };
